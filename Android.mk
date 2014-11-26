959d3362451fe081d9a25fde87e989ffe90ab159
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-26 03:14:15 +0000

    Do not include common SE policies automatically
    Automatic additions to BOARD_SEPOLICY_UNION from Android.mk do not
    guarantee order or precedence of rules. More often than not, these
    rules get included _after_ the device's own, which makes
    it impossible for the device to extend them, or even to use types
    defined here.
    
    Manually include these in the device's BoardConfig with
    "include device/qcom/sepolicy/sepolicy.mk" before
    its own specific BOARD_SEPOLICY_DIRS (if any) instead. Or just
    include files piecemeal.
    
    Change-Id: I2ce0c3ec325906a46570b26c5f0c2018390d2ec9

--- Android.mk
+++ Android.mk
@@ -1,11 +1,10 @@
-# Board specific SELinux policy variable definitions
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-LOCAL_PATH:= $(call my-dir)
-BOARD_SEPOLICY_DIRS := \
-       $(BOARD_SEPOLICY_DIRS) \
-       $(LOCAL_PATH) \
-       $(LOCAL_PATH)/common \
-       $(LOCAL_PATH)/test \
-       $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)
-
-endif
+# Don't recurse into the platform makefiles. We don't care about them, and
+# we don't want to force a reset of BOARD_SEPOLICY_DIRS
+#
+# If you want to use these policies, add a
+# 
+# include device/qcom/sepolicy/sepolicy.mk
+#
+# to your device's BoardConfig. It is highly recommended that in case
+# you have your own BOARD_SEPOLICY_DIRS and BOARD_SEPOLICY_UNION declarations,
+# the inclusion happens _before_ those lines
