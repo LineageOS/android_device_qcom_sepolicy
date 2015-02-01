tusharind007/sepolicy_vndr

SEandroid: Ultrasound: Add policy for ultrasound
Adds policies for the ultrasound daemons,
digital pen system service and system applications

Change-Id: I9193a8625d1e0e707a825a63981b02fc9924a043

Android.mk
@@ -93,7 +93,8 @@ BOARD_SEPOLICY_UNION := \
        hbtp.te \
        kernel.te \
        vold.te \
-       energyawareness.te
+       energyawareness.te \
+       usf.te
 
 # Compile sensor pilicy only for SSC targets
 SSC_TARGET_LIST := apq8084

LineageOS/android_device_qcom_sepolicy

Android.mk
@@ -94,7 +94,8 @@ BOARD_SEPOLICY_UNION := \
        hbtp.te \
        kernel.te \
        vold.te \
-       wfdservice.te
+       wfdservice.te \
+       usf.te
 
 # Compile sensor pilicy only for SSC targets
 SSC_TARGET_LIST := apq8084