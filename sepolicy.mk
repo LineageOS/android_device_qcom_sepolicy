25350630bef59647f6466a06177dc066f96600b7
Steve Kondik <steve@cyngn.com>
2015-10-31 06:33:29 -0700

    sepolicy: Include CM+QCOM SEpolicy
    Change-Id: I6b7f1716f9850e93145d9d379df1ffc0d97e19a2

--- sepolicy.mk
+++ sepolicy.mk
@@ -5,3 +5,5 @@ BOARD_SEPOLICY_DIRS := \
        device/qcom/sepolicy/common \
        device/qcom/sepolicy/test \
        device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+
+-include vendor/cm/sepolicy/qcom/sepolicy.mk
