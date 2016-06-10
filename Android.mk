588d6831574bb31b28e0b6d30990386d8725261c
Ajit Kumar <kajit@codeaurora.org>
2016-06-10 19:25:46 +0530

    Resolve compilation issue.
    Updated the path to resolve compilation issue.
    
    Change-Id: Ice6ad2e2c83ad67443bca66feb4b5269a817abdb

--- Android.mk
+++ Android.mk
@@ -1,9 +1,11 @@
 # Board specific SELinux policy variable definitions
 ifeq ($(call is-vendor-board-platform,QCOM),true)
+LOCAL_PATH:= $(call my-dir)
 BOARD_SEPOLICY_DIRS := \
        $(BOARD_SEPOLICY_DIRS) \
-       device/qcom/sepolicy \
-       device/qcom/sepolicy/common \
-       device/qcom/sepolicy/test \
-       device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
+       $(LOCAL_PATH) \
+       $(LOCAL_PATH)/common \
+       $(LOCAL_PATH)/test \
+       $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)
+
 endif
