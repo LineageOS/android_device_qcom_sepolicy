4ace41a15ae9c0c01aaedda4bb48c0179b139fed
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-29 20:58:26 -0700

    sepolicy: Move definitions from Android.mk to make opt-in
    Move the BOARD definitions to sepolicy.mk to allow devices to opt
    in rather then automatically include with Android.mk. Also include
    lineage-specific qcom sepolicy now located in
    device/lineage/sepolicy/qcom.
    
    Original commits:
    https://github.com/LineageOS/android_device_qcom_sepolicy/commit/959d3362451fe081d9a25fde87e989ffe90ab159
    https://github.com/LineageOS/android_device_qcom_sepolicy/commit/27f078f1041497ecadf1315b39f130882177ea64
    
    Change-Id: Ibf70e4c8ab9d91b50c62c3e9f1263e1624e8ca00

--- Android.mk
+++ Android.mk
@@ -1,34 +1,10 @@
-# Board specific SELinux policy variable definitions
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-LOCAL_PATH:= $(call my-dir)
-BOARD_SEPOLICY_DIRS := \
-       $(BOARD_SEPOLICY_DIRS) \
-       $(LOCAL_PATH) \
-       $(LOCAL_PATH)/vendor/common \
-       $(LOCAL_PATH)/vendor/ssg \
-       $(LOCAL_PATH)/vendor/common/sysmonapp
-
-ifeq ($(TARGET_SEPOLICY_DIR),)
-BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/vendor/$(TARGET_BOARD_PLATFORM)
-else
-BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/vendor/$(TARGET_SEPOLICY_DIR)
-endif
-
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-BOARD_SEPOLICY_DIRS += \
-       $(LOCAL_PATH)/vendor/test
-endif
-
-ifneq (,$(filter 24 25 26 27, $(PRODUCT_SHIPPING_API_LEVEL)))
-BOARD_SEPOLICY_DIRS += \
-       $(LOCAL_PATH)/vendor/ota/$(TARGET_BOARD_PLATFORM)
-endif
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/public
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/private
-endif
+# Don't recurse into the platform makefiles. We don't care about them, and
+# we don't want to force a reset of BOARD_SEPOLICY_DIRS
+#
+# If you want to use these policies, add a
+#
+# include device/qcom/sepolicy/sepolicy.mk
+#
+# to your device's BoardConfig. It is highly recommended that in case
+# you have your own BOARD_SEPOLICY_DIRS declaration,
+# the inclusion happens _before_ those lines
