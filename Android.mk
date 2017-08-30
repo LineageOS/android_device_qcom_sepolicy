b95332942234fd2b621b54068d4bff006bc702dc
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-29 20:58:26 -0700

    sepolicy: Move definitions from Android.mk to make opt-in
    Move the BOARD definitions to sepolicy.mk to allow devices to opt
    in rather then automatically include with Android.mk. Also include
    lineage-specific qcom sepolicy now located in
    device/lineage/sepolicy/qcom.
    
    Original commits:
    https://github.com/LineageOS/android_device_qcom_sepolicy/commit/959d3362451fe081d9a25fde87e989ffe90ab159
    https://github.com/LineageOS/android_device_qcom_sepolicy/commit/27f078f1041497ecadf1315b39f130882177ea64
    
    Change-Id: Ibf70e4c8ab9d91b50c62c3e9f1263e1624e8ca00

--- Android.mk
+++ Android.mk
@@ -1,26 +1,10 @@
-# Board specific SELinux policy variable definitions
-ifeq ($(call is-vendor-board-platform,QCOM),true)
-LOCAL_PATH:= $(call my-dir)
-BOARD_SEPOLICY_DIRS := \
-       $(BOARD_SEPOLICY_DIRS) \
-       $(LOCAL_PATH) \
-       $(LOCAL_PATH)/common \
-       $(LOCAL_PATH)/ssg \
-       $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)
-
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-BOARD_SEPOLICY_DIRS += \
-       $(LOCAL_PATH)/test
-endif
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/public
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/private
-
-# Add sepolicy version to support OS upgrade and backward compatibility
-BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
-endif
+# Don't recurse into the platform makefiles. We don't care about them, and
+# we don't want to force a reset of BOARD_SEPOLICY_DIRS
+#
+# If you want to use these policies, add a
+#
+# include device/qcom/sepolicy/sepolicy.mk
+#
+# to your device's BoardConfig. It is highly recommended that in case
+# you have your own BOARD_SEPOLICY_DIRS declaration,
+# the inclusion happens _before_ those lines
