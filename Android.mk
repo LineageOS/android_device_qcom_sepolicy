3fd09c4fd06feb1b147dcc2c6a79d8247830c5bd
Tharun Kumar Merugu <mtharu@codeaurora.org>
2018-07-20 23:50:44 +0530

    sepolicy: add domain for sysmonapp applications
    Add a domain with necessary permissions for sysmonapp applications
    signed using the certificate provided.
    
    Change-Id: Id61eb7e263cf83724305eda624fb06c2df5ac555

--- Android.mk
+++ Android.mk
@@ -5,7 +5,8 @@ BOARD_SEPOLICY_DIRS := \
        $(BOARD_SEPOLICY_DIRS) \
        $(LOCAL_PATH) \
        $(LOCAL_PATH)/vendor/common \
-       $(LOCAL_PATH)/vendor/$(TARGET_BOARD_PLATFORM)
+       $(LOCAL_PATH)/vendor/$(TARGET_BOARD_PLATFORM) \
+       $(LOCAL_PATH)/vendor/common/sysmonapp
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
     $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
