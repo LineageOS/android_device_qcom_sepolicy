78f021fe6a34840c877d4c06d497b71d9c31735e
Jaihind Yadav <jaihindyadav@codeaurora.org>
2019-01-25 15:44:50 +0530

    sepolicy: moving qssi supported legacy target here.
    Change-Id: Ife7e851823afc1dcbf2f561c8079795e909544bc

--- Android.mk
+++ Android.mk
@@ -1,28 +1,50 @@
 # Board specific SELinux policy variable definitions
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 LOCAL_PATH:= $(call my-dir)
-BOARD_SEPOLICY_DIRS := \
-       $(BOARD_SEPOLICY_DIRS) \
-       $(LOCAL_PATH) \
-       $(LOCAL_PATH)/qva/vendor/common/sysmonapp \
-       $(LOCAL_PATH)/generic/vendor/common \
-       $(LOCAL_PATH)/generic/vendor/$(TARGET_BOARD_PLATFORM) \
-       $(LOCAL_PATH)/qva/vendor/$(TARGET_BOARD_PLATFORM) \
-       $(LOCAL_PATH)/qva/vendor/common \
-       $(LOCAL_PATH)/qva/vendor/ssg
-
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
     $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/generic/public \
-     $(LOCAL_PATH)/qva/public
+    $(LOCAL_PATH)/generic/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
     $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    $(LOCAL_PATH)/generic/private \
+    $(LOCAL_PATH)/generic/private
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
+    $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
+    $(LOCAL_PATH)/qva/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
+    $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
     $(LOCAL_PATH)/qva/private
 
-ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
-BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/test
+ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
+    BOARD_SEPOLICY_DIRS := \
+       $(BOARD_SEPOLICY_DIRS) \
+       $(LOCAL_PATH) \
+       $(LOCAL_PATH)/generic/vendor/common \
+       $(LOCAL_PATH)/generic/vendor/$(TARGET_BOARD_PLATFORM) \
+       $(LOCAL_PATH)/qva/vendor/common/sysmonapp \
+       $(LOCAL_PATH)/qva/vendor/$(TARGET_BOARD_PLATFORM) \
+       $(LOCAL_PATH)/qva/vendor/ssg \
+       $(LOCAL_PATH)/qva/vendor/common
+
+
+    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+    BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/generic/vendor/test
+    endif
 endif
 
+ifneq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
+    BOARD_SEPOLICY_DIRS := \
+                 $(BOARD_SEPOLICY_DIRS) \
+                 $(LOCAL_PATH) \
+                 $(LOCAL_PATH)/legacy/vendor/common/sysmonapp \
+                 $(LOCAL_PATH)/legacy/vendor/$(TARGET_BOARD_PLATFORM) \
+                 $(LOCAL_PATH)/legacy/vendor/ssg \
+                 $(LOCAL_PATH)/legacy/vendor/common
+
+    ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
+    BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/legacy/vendor/test
+    endif
+endif
 endif
