bb14bea11440219bb597503f54e2bbe93d1f8ccb
Jaihind Yadav <jaihindyadav@codeaurora.org>
2019-09-17 11:09:28 +0530

    sepolicy: adding support for product and odm.
    Change-Id: I517f1914412d5977bda1f5cdc47506e8be58b09d

--- Android.mk
+++ Android.mk
@@ -17,6 +17,15 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
     $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
     $(LOCAL_PATH)/qva/private
 
+# sepolicy rules for product images
+PRODUCT_PUBLIC_SEPOLICY_DIRS := \
+    $(PRODUCT_PUBLIC_SEPOLICY_DIRS) \
+     $(LOCAL_PATH)/product/public
+
+PRODUCT_PRIVATE_SEPOLICY_DIRS := \
+    $(PRODUCT_PRIVATE_SEPOLICY_DIRS) \
+     $(LOCAL_PATH)/product/private
+
 ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS := \
        $(BOARD_SEPOLICY_DIRS) \
