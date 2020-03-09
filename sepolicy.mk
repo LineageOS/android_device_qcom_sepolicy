aae8cce02b052f9150382ea5d0c0d7d861b3dc1b
Michael Bestas <mkbestas@lineageos.org>
2020-03-09 18:25:50 +0200

    Merge tag 'LA.UM.8.1.r1-14300-sm8150.0' of https://source.codeaurora.org/quic/la/device/qcom/sepolicy into lineage-17.1
    "LA.UM.8.1.r1-14300-sm8150.0"
    
    Conflicts:
            Android.mk
    
    Change-Id: Idcc9b87e9000c1f64c0048f153286ee67e0be343

--- sepolicy.mk
+++ sepolicy.mk
@@ -11,12 +11,20 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy/qva/private
 
+# sepolicy rules for product images
+PRODUCT_PUBLIC_SEPOLICY_DIRS += \
+    device/qcom/sepolicy/product/public
+
+PRODUCT_PRIVATE_SEPOLICY_DIRS += \
+    device/qcom/sepolicy/product/private
+
 ifeq (,$(filter sdm845 sdm710, $(TARGET_BOARD_PLATFORM)))
     BOARD_SEPOLICY_DIRS += \
         device/qcom/sepolicy/generic/vendor/common \
         device/qcom/sepolicy/qva/vendor/common/sysmonapp \
         device/qcom/sepolicy/qva/vendor/ssg \
         device/qcom/sepolicy/timeservice \
+        device/qcom/sepolicy/qva/vendor/qwesas \
         device/qcom/sepolicy/qva/vendor/common
 
     ifeq ($(TARGET_SEPOLICY_DIR),)
