7d42dff1e446a10fb775838b746071abf2c2c524
Jaihind Yadav <jaihindyadav@codeaurora.org>
2020-11-24 13:28:46 +0530

    moving changes from qva to generic
    1- as system side vendor customization can go to product/system_ext
    partition so i think there is no need to keep qva so moving the
    changes from qva to generic.
    
    2- adding prebuilts dir for system_ext and product.
    
    Change-Id: I5164a313f7c784e0948ab933dfa6a9581a94b863

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -1,34 +1,24 @@
 # Board specific SELinux policy variable definitions
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 SEPOLICY_PATH:= device/qcom/sepolicy
+BOARD_SYSTEM_EXT_PREBUILT_DIR := device/qcom/sepolicy/generic
+BOARD_PRODUCT_PREBUILT_DIR := device/qcom/sepolicy/generic/product
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
     $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    $(SEPOLICY_PATH)/generic/public \
-    $(SEPOLICY_PATH)/generic/public/attribute
+    $(SEPOLICY_PATH)/generic/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
     $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
     $(SEPOLICY_PATH)/generic/private
 
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    $(SEPOLICY_PATH)/qva/public \
-    $(SEPOLICY_PATH)/qva/public/attribute
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    $(SEPOLICY_PATH)/qva/private
-
 #once all the services are moved to Product /ODM above lines will be removed.
 # sepolicy rules for product images
 PRODUCT_PUBLIC_SEPOLICY_DIRS := \
     $(PRODUCT_PUBLIC_SEPOLICY_DIRS) \
-    $(SEPOLICY_PATH)/generic/product/public \
-    $(SEPOLICY_PATH)/qva/product/public 
+    $(SEPOLICY_PATH)/generic/product/public
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS := \
     $(PRODUCT_PRIVATE_SEPOLICY_DIRS) \
-    $(SEPOLICY_PATH)/generic/product/private \
-    $(SEPOLICY_PATH)/qva/product/private
+    $(SEPOLICY_PATH)/generic/product/private
 
 endif
