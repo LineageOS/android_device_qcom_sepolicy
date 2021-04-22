07828b6176cf8342a75da3cfe5d7cf7d6820a145
Jaihind Yadav <jaihindyadav@codeaurora.org>
2021-04-22 20:39:34 +0530

    adding required infra to test treblesepolicy test.
    treblesepolicy test requires these *.cil file to
    be set by partner so adding required changes to do
    so.
    
    Change-Id: I0691776ceae5921d0c9254eda62790ae161968c0

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -1,8 +1,12 @@
 # Board specific SELinux policy variable definitions
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 SEPOLICY_PATH:= device/qcom/sepolicy
+LOCAL_PATH := $(call my-dir)
 BOARD_SYSTEM_EXT_PREBUILT_DIR := device/qcom/sepolicy/generic
 BOARD_PRODUCT_PREBUILT_DIR := device/qcom/sepolicy/generic/product
+BOARD_PLAT_PUB_VERSIONED_POLICY := device/qcom/sepolicy
+$(shell $(SEPOLICY_PATH)/append.sh)
+
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS := \
     $(SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS) \
     $(SEPOLICY_PATH)/generic/public
